b675459629c2d59a85689c7742527a10a56debf0
Arden Ranta <arranta20@gmail.com>
2023-07-31 20:40:57 -0700

    Update the fork with a second completed assignment.

--- ifStatements.js
+++ ifStatements.js
@@ -8,11 +8,13 @@
 // Write a conditional statement that prints only one or the other.
 // Experiment with manipulating the value held within numQuarters to make sure both conditions can be achieved.
 
-var numQuarters = 0;
+var numQuarters = 3;
 
+if (numQuarters >= 2){
 console.log("I have enough money for a gumball");
+} else {
 console.log("I don't have enough money for a gumball");
-
+}
 
 // CHALLENGE 2
 // Using the variables defined below, write code that will tell you if you have the ingredients to make a pizza. 
@@ -29,3 +31,8 @@ console.log("I don't have enough money for a gumball");
 var cupsOfFlour = 1;
 var hasSauce = true;
 
+if (cupsOfFlour >= 2 && hasSauce === true){
+    console.log("I can make pizza")
+} else {
+    console.log("I cannot make pizza")
+}
